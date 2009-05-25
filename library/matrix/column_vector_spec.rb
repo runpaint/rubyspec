3f9c2ac11d3a9ef81451a5757d497bc3b88c6035
Run Paint Run Run <runrun@runpaint.org>
2009-05-25 15:53:25 +0100

    Matrix.column_vector returns single-col. Matrix.

--- library/matrix/column_vector_spec.rb
+++ library/matrix/column_vector_spec.rb
@@ -2,5 +2,24 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'matrix'
 
 describe "Matrix.column_vector" do
-  it "needs to be reviewed for spec completeness"
+
+  it "returns a single column Matrix when called with an Array" do
+    @m = Matrix.column_vector([4,5,6])
+    @m.instance_of?(Matrix).should be_true
+    @m.should == Matrix[ [4],[5],[6] ]
+
+    @m = Matrix.column_vector([])
+    @m.instance_of?(Matrix).should be_true
+    @m.should == Matrix[]
+  end
+
+  it "returns a single column Matrix when called with a number" do
+    @m = Matrix.column_vector(4)
+    @m.instance_of?(Matrix).should be_true
+    @m.should == Matrix[ [4] ]
+
+    @m = Matrix.column_vector(0.98887)
+    @m.instance_of?(Matrix).should be_true
+    @m.should == Matrix[ [0.98887] ]
+  end
 end
