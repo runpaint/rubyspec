13a1045f1e5d1a13c878a30d61efc0700528f580
Run Paint Run Run <runrun@runpaint.org>
2009-05-25 15:17:38 +0100

    Matrix#columns returns an instance of Matrix.

--- library/matrix/columns_spec.rb
+++ library/matrix/columns_spec.rb
@@ -8,7 +8,8 @@ describe "Matrix.columns" do
     @m = Matrix.columns([@a, @b])
   end
   
-  it "creates a matrix from argument columns" do
+  it "creates a Matrix from argument columns" do
+    @m.instance_of?(Matrix).should be_true
     @m.column(0).to_a.should == @a
     @m.column(1).to_a.should == @b
   end
