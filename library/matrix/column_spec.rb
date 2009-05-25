a107eccb141d76e0a224c2040e294fd892560309
Run Paint Run Run <runrun@runpaint.org>
2009-05-25 14:33:42 +0100

    Matrix#column returns nil Vector on invalid column.
    When Matrix#column is called with an invalid column vector it returns a Vector
    whose elements are nil.

--- library/matrix/column_spec.rb
+++ library/matrix/column_spec.rb
@@ -8,11 +8,15 @@ describe "Matrix#column" do
 
   it "returns a Vector when called without a block" do
     Matrix[ *@data ].column(1).should == Vector[2,2]
+    Matrix[ *@data ].column(2).should == Vector[nil,nil]
   end
 
   it "yields each element in the column to the block" do
     Matrix[ *@data ].column(1) do |n|
       n.should == 2
+    end
+    Matrix[ *@data ].column(2) do |n|
+      n.should == nil
     end
   end
 end
