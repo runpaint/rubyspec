8b6d1041812532d01cd8377b72c13f6d29171eb2
Run Paint Run Run <runrun@runpaint.org>
2009-05-25 17:12:52 +0100

    Matrix#column_vectors returns Array of Vectors.

--- library/matrix/column_vectors_spec.rb
+++ library/matrix/column_vectors_spec.rb
@@ -2,5 +2,13 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'matrix'
 
 describe "Matrix#column_vectors" do
-  it "needs to be reviewed for spec completeness"
+  it "returns an Array of column Vectors" do
+    @m = Matrix[ *[ [1,2], [3,4] ] ]
+    @v = @m.column_vectors
+    @v.instance_of?(Array).should be_true
+    @v.all? {|v| v.instance_of?(Vector).should be_true }
+    @v.should == [Vector[1,3], Vector[2,4]]
+
+    Matrix[ [] ].column_vectors.should == []
+  end
 end
